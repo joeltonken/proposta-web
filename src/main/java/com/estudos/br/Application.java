61f194c76f96d542d2717dfec488ef37a6318dba
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-10-20 15:58:04 -0300

    adiciona banner e ajustes no application.properties

--- src/main/java/com/estudos/br/Application.java
+++ src/main/java/com/estudos/br/Application.java
@@ -1,13 +1,13 @@
-package com.estudos.br.proposta_web;
+package com.estudos.br;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PropostaWebApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PropostaWebApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/estudos/br/PropostaWebApplicationTests.java
+++ src/test/java/com/estudos/br/PropostaWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.estudos.br.proposta_web;
+package com.estudos.br;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
