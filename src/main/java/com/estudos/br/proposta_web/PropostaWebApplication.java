99ebead50801a363a506e6129f199c3bcfb14f04
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-10-20 14:47:44 -0300

    commit inicial

---
+++ src/main/java/com/estudos/br/proposta_web/PropostaWebApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.br.proposta_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/br/proposta_web/PropostaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.br.proposta_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
