377257fd0b951733c52027b01dda91a86f14933d
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-10-22 22:34:22 -0300

    ajuste de boas práticas de REST

--- src/main/java/com/estudos/br/controllers/PropostaController.java
+++ src/main/java/com/estudos/br/controllers/PropostaController.java
@@ -1,24 +1,30 @@
 package com.estudos.br.controllers;
 
 import com.estudos.br.dtos.PropostaRequestDTO;
+import com.estudos.br.dtos.PropostaResponseDTO;
 import com.estudos.br.services.PropostaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/proposta")
 public class PropostaController {
 
-    @Autowired
-    private PropostaService service;
+    private final PropostaService service;
 
     @PostMapping
     public ResponseEntity criar(@RequestBody PropostaRequestDTO dto) {
-        return ResponseEntity.ok(service.criar(dto));
+        PropostaResponseDTO response = service.criar(dto);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri()).body(response);
     }
 
 }
